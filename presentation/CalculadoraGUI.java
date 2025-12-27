4572e391563e6c2cc0500d9fcc455423a035f435
mbquial <pepitotolitis@gmail.com>
2025-12-26 21:41:04 -0500

    feat: add board (part with numbers and that stuff)

--- presentation/CalculadoraGUI.java
+++ presentation/CalculadoraGUI.java
@@ -5,9 +5,9 @@ import java.awt.*;
 public class CalculadoraGUI extends JFrame{
     private int width, height;
     
-    private JButton oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, zeroButton, dotButton, equalButton, addButton, minusButton, multButton, divButton;
-    private JPanel panel;
-    private GridLayout gridLayout;
+    private JButton oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, zeroButton, dotButton, equalButton, addButton, minusButton, multButton, divButton, parenLeftButton, parenRightButton, percentageButton, acButton;
+    private JPanel buttonPanel;
+    private JTextField numbersField;
     
 
     public CalculadoraGUI(){
@@ -24,7 +24,8 @@ public class CalculadoraGUI extends JFrame{
         setLocation((dimensions.width - width)/6, (dimensions.height - height)/6);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        panel = new JPanel();
+        buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(5,4));
 
         oneButton = new JButton("1");
         twoButton = new JButton("2");
@@ -39,9 +40,47 @@ public class CalculadoraGUI extends JFrame{
         dotButton = new JButton(",");
         addButton = new JButton("+");
         minusButton = new JButton("-");
-        multButton = new JButton("*"); // O podría cambiarla por X
-        divButton = new JButton("/"); // O podría cambiarla por el signo que tiene las dos bolitas
+        multButton = new JButton("X");
+        divButton = new JButton("/"); 
         equalButton = new JButton("=");
+        parenLeftButton = new JButton("(");
+        parenRightButton = new JButton(")");
+        percentageButton = new JButton("%");
+        acButton = new JButton("AC");
+
+        buttonPanel.add(parenLeftButton);
+        buttonPanel.add(parenRightButton);
+        buttonPanel.add(percentageButton);
+        buttonPanel.add(acButton);
+        
+        buttonPanel.add(sevenButton);
+        buttonPanel.add(eightButton);
+        buttonPanel.add(nineButton);
+        buttonPanel.add(divButton);
+
+        buttonPanel.add(fourButton);
+        buttonPanel.add(fiveButton);
+        buttonPanel.add(sixButton);
+        buttonPanel.add(multButton);
+
+        buttonPanel.add(oneButton);
+        buttonPanel.add(twoButton);
+        buttonPanel.add(threeButton);
+        buttonPanel.add(minusButton);
+
+        buttonPanel.add(zeroButton);
+        buttonPanel.add(dotButton);
+        buttonPanel.add(equalButton);
+        buttonPanel.add(addButton);
+
+        numbersField = new JTextField();
+        numbersField.setEditable(false);
+        numbersField.setFont(new Font("Arial", Font.BOLD, 24));
+        numbersField.setHorizontalAlignment(JTextField.RIGHT);
+
+        setLayout(new BorderLayout());
+        add(numbersField, BorderLayout.NORTH);
+        add(buttonPanel, BorderLayout.CENTER);
     }
     public static void main(String[] args){
         CalculadoraGUI gui = new CalculadoraGUI();
